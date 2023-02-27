bfa40041a42b47c4d9cbcc24f9057dfb791275c9
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-26 21:26:20 -0500

    comenzando Herencia

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,30 +1,14 @@
-public class Gerente {
-    private String nombre;
-    private String documento;
-    private double salario;
-    //private double bonificacion;   
+public class Gerente extends Funcionario {
+    private String clave;
 
-    public Gerente() {
-
+    public void setClave(String clave){
+        this.clave = clave;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getDocumento() {
-        return documento;
-    }
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-    public double getSalario() {
-        return salario;
-    }
-    public void setSalario(double salario) {
-        this.salario = salario;
+    public String getClave() {
+        return clave;
     }
 
+    public boolean inciarSesion(String clave){
+        return clave == "Willfredo";
+    }
 }
--- src/TestGerente.java
+++ src/TestGerente.java
@@ -1,10 +1,15 @@
 public class TestGerente {
     public static void main(String[] args) {
-        Funcionario gerente = new Funcionario();
+        //Funcionario gerente = new Funcionario(); //Siempre en cuando funcionario sea el padre
+        Gerente gerente = new Gerente(); // accedo a la clase hija Gerente
+        gerente.setNombre("oscar");
         gerente.setSalario(6000);
         gerente.setTipo(1);
+        gerente.setClave("Willfredo");
           
         System.out.println(gerente.getSalario());
         System.out.println(gerente.getBonificacion());
+        System.out.println(gerente.getClave());
+        System.out.println(gerente.inciarSesion("Willfredo"));
     }
 }
