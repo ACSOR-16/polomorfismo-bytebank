127a7a3f5c10cdc0e5634de4ad13347688383869
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-27 19:08:36 -0500

    herencia y el uso de constructores

---
+++ src/Cliente.java
@@ -0,0 +1,27 @@
+public class Cliente {
+    
+    private String telefono;
+    private String nombre;
+    private String documento;
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+    public String getDocumento() {
+        return documento;
+    }
+}
---
+++ src/Cuenta.java
@@ -0,0 +1,67 @@
+class Cuenta{
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular = new Cliente();
+
+    private static int total; 
+
+    public Cuenta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        total ++;
+    }
+
+    public void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+        // this.saldo += valor;
+    }
+    
+    public boolean retirar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean transferir(double valor, Cuenta cuentaDestino){
+        if (this.saldo >= valor) {
+            this.retirar(valor);
+            cuentaDestino.depositar(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public double getSaldo() {
+        return this.saldo;
+    }
+    
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setNumero(int numero) {
+        if (numero > 0) {
+            this.numero = numero;
+        }
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+    public Cliente getTitular() {
+        return titular;
+    }
+    
+    public static int getTotal() {
+        return total;
+        //para accedet a total parte de la clase y no de la instancia
+    }
+}
